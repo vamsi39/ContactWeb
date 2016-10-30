adeba825294a288e547aa99528e1d15f82fff24f
Vamsi <vamsi.pabbineedi@gmail.com>
2016-10-30 16:42:10 +0000

    Initial Commit

---
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContactWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        // Order it will generate/render
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib/jquery.min.js"));
+
+             
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/lib/bootstrap.min.js"));
+                  
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib/jquery.dataTables.min.js",
+                        "~/js/lib/dataTables.bootstrap.min.js",
+                        "~/js/lib/dataTables.colReorder.min.js"));
+
+
+             
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/jquery.dataTables.min.css",
+                      "~/css/jquery.dataTables_themeroller.css",
+                      "~/css/dataTables_bootstrap.min.css",
+                      "~/css/colReorder.bootstrap.min.css",
+                      "~/css/site.css"));
+        }
+    }
+}
