40939bf1a936953600d2dd894f3f466ec0374840
Vamsi <vamsi.pabbineedi@gmail.com>
2016-11-01 21:26:50 +0000

    Added Contact model to the solution Also added database migrations

---
+++ ContactWeb/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactWeb.Models
+{
+    public class Contact         // contact object with each property 
+    {
+        public int id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhonePrimary { get; set; }
+        public string PhoneSecondary { get; set; }
+        public DateTime Birthday { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+
+
+    }
+}
